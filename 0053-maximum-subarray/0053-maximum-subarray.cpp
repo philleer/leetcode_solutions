c9abae51bb642ed5a5a4016a6c55577c99899ae0
phillee <phillee2016@163.com>
2026-04-10 11:39:56 +0800

    [chore] [Medium]_0053-maximum-subarray_.cpp_Array_04-10-2026

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
+    int recursive(std::vector<int> &nums, int left, int right) {
+        if (left == right) return nums[left];
+
+        int mid = left + (right - left) / 2;
+        int left_max = INT_MIN;
+        int sum = 0;
+        for (int i = mid; i >= left; i--) {
+            sum += nums[i];
+            left_max = std::max(sum, left_max);
+        }
+
+        sum = 0;
+        int right_max = INT_MIN;
+        for (int i = mid + 1; i <= right; i++) {
+            sum += nums[i];
+            right_max = std::max(sum, right_max);
+        }
+
+        return std::max(left_max + right_max, std::max(recursive(nums, left, mid), recursive(nums, mid + 1, right)));
+    }
+
     int maxSubArray(vector<int>& nums) {
         if (nums.empty()) return 0;
         if (nums.size() == 1) return nums.back();
 
-        int max_sum = INT_MIN;
         int sz = nums.size();
-        std::vector<int> dp(sz, INT_MIN);
-        dp[0] = nums[0];
-        for (int i = 1; i < sz; i++) {
-            dp[i] = std::max(dp[i - 1] + nums[i], nums[i]);
-            if (dp[i] > max_sum) {
-                max_sum = dp[i];
-            }
-        }
-        max_sum = std::max(dp[0], max_sum);
 
-        return max_sum;
+        // Solution (1): dynamic programming method, time complexity O(n), space complexity O(n)
+        // std::vector<int> dp(sz, INT_MIN);
+        // dp[0] = nums[0];
+        // int max_sum = nums[0];
+        // for (int i = 1; i < sz; i++) {
+        //     dp[i] = std::max(dp[i - 1] + nums[i], nums[i]);
+        //     if (dp[i] > max_sum) {
+        //         max_sum = dp[i];
+        //     }
+        // }
+
+        // return max_sum;
+
+        // Solution (2): divide and conquer method
+        return recursive(nums, 0, sz - 1);
     }
 };
