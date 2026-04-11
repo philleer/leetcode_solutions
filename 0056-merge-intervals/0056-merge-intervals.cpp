761d40e4c0a5e0cb0ec072f68afc1524b85e23c5
phillee <phillee2016@163.com>
2026-04-11 15:59:26 +0800

    [chore] [Medium]_0056-merge-intervals_.cpp_Array_04-11-2026

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,43 +5,33 @@ public:
         if (intervals.size() == 1) return intervals;
 
         int sz = intervals.size();
-        std::vector<std::pair<int, int>> begin_intervals(sz);
-        std::vector<std::pair<int, int>> end_intervals;
-        for (int i = 0; i < sz; i++) {
-            begin_intervals[i] = {intervals[i][0], intervals[i][1]};
-        }
-        end_intervals.assign(begin_intervals.begin(), begin_intervals.end());
-        std::sort(begin_intervals.begin(), begin_intervals.end(), [] (std::pair<int, int> &a, std::pair<int, int> &b) {
-            return ((a.first != b.first) ? (a.first < b.first) : (a.second < b.second));
-        });
-        std::sort(end_intervals.begin(), end_intervals.end(), [] (std::pair<int, int> &a, std::pair<int, int> &b) {
-            return ((a.first != b.first) ? (a.first < b.first) : (a.second < b.second));
+        std::sort(intervals.begin(), intervals.end(), [] (std::vector<int> &a, std::vector<int> &b) {
+            return ((a[0] != b[0]) ? (a[0] < b[0]) : (a[1] < b[1]));
         });
 
         int left = INT_MAX;
         int right = INT_MIN;
         std::vector<std::vector<int>> results;
         for (int i = 0; i < sz; i++) {
-            if (begin_intervals[i].second <= right) continue;
-            if (begin_intervals[i].first < left || begin_intervals[i].first > right) {
-                left = begin_intervals[i].first;
+            if (intervals[i][1] <= right) continue;
+            if (intervals[i][0] < left || intervals[i][0] > right) {
+                left = intervals[i][0];
             }
-            if (begin_intervals[i].second > right || begin_intervals[i].first > right) {
-                right = begin_intervals[i].second;
+            if (intervals[i][1] > right || intervals[i][0] > right) {
+                right = intervals[i][1];
             }
-            for (int j = 0; j < sz; j++) {
-                if (end_intervals[j].second <= left) continue;
-                if (end_intervals[j].first <= right) {
-                    right = std::max(end_intervals[j].second, right);
+            for (int j = i + 1; j < sz; j++) {
+                if (intervals[j][1] <= left) continue;
+                if (intervals[j][0] <= right) {
+                    right = std::max(intervals[j][1], right);
                 } else {
                     results.emplace_back(std::vector<int>({left, right}));
                     break;
                 }
             }
         }
-        if (!results.empty() && (results.back()[0] != left || results.back()[1] != right)) {
-            results.emplace_back(std::vector<int>({left, right}));
-        } else if (results.empty() && left != INT_MAX && right != INT_MIN) {
+        if ((!results.empty() && (results.back()[0] != left || results.back()[1] != right)) ||
+            (results.empty() && left != INT_MAX && right != INT_MIN)) {
             results.emplace_back(std::vector<int>({left, right}));
         }
 
