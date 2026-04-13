3273b5699a0d591c8188525501940fcf1ad23062
phillee <phillee2016@163.com>
2026-04-13 11:47:58 +0800

    [chore] [Medium]_0064-minimum-path-sum_.cpp_Array_04-13-2026

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -4,37 +4,23 @@ public:
         if (grid.empty()) return 0;
 
         int m = grid.size();
-        int sum = 0;
-        if (m == 1) {
-            for (int c = 0; c < grid[0].size(); c++) sum += grid[0][c];
-            return sum;
-        }
-
         int n = grid[0].size();
-        if (n == 1) {
-            sum = 0;
-            for (int r = 0; r < m; r++) sum += grid[r][0];
-            return sum;
-        }
-        
-        std::vector<std::vector<int>> dp(m, std::vector<int>(n, INT_MAX));
-        sum = 0;
-        for (int r = 0; r < m; r++) {
-            sum += grid[r][0];
-            dp[r][0] = std::min(sum, dp[r][0]);
-        }
-        sum = 0;
-        for (int c = 0; c < n; c++) {
-            sum += grid[0][c];
-            dp[0][c] = std::min(sum, dp[0][c]);
-        }
 
-        for (int r = 1; r < m; r++) {
-            for (int c = 1; c < n; c++) {
-                dp[r][c] = std::min(dp[r][c - 1], dp[r - 1][c]) + grid[r][c];
+        std::vector<int> dp(n, 0);
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (r == 0 && c == 0) {
+                    dp[c] = grid[r][c];
+                } else if (r == 0) {
+                    dp[c] = dp[c - 1] + grid[r][c];
+                } else if (c == 0) {
+                    dp[c] = dp[c] + grid[r][c];
+                } else {
+                    dp[c] = std::min(dp[c], dp[c - 1]) + grid[r][c];
+                }
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 };
