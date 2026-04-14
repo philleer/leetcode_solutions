7622976ef7907855bee9c7f39e20e651f25dbd17
phillee <phillee2016@163.com>
2026-04-14 11:48:29 +0800

    [chore] [Easy]_0070-climbing-stairs_.cpp_Math_04-14-2026

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,13 +3,15 @@ public:
     int climbStairs(int n) {
         if (n <= 2) return n;
 
-        std::vector<int> dp(n, 0);
-        dp[0] = 1;
-        dp[1] = 2;
+        int last_two = 1;
+        int last_one = 2;
+        int cnt = 0;
         for (int i = 2; i < n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            cnt = last_one + last_two;
+            last_two = last_one;
+            last_one = cnt;
         }
 
-        return dp[n - 1];
+        return cnt;
     }
 };
