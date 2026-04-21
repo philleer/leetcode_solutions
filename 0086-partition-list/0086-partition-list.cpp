6bcc7685e66631fe2ac6d01223f58434af0a9401
phillee <phillee2016@163.com>
2026-04-22 01:07:04 +0800

    [chore] [Medium]_0086-partition-list_.cpp_Linked List_04-22-2026

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        if (!head || !head->next) return head;
+
+        ListNode *cur = head;
+        ListNode *new_head = new ListNode(-1);
+        ListNode *pre = new_head;
+        ListNode *dummy = new ListNode(-1);
+        ListNode *sub = dummy;
+        while (cur) {
+            if (cur->val < x) {
+                pre->next = cur;
+                pre = pre->next;
+                // std::cout << "pre->val = " << pre->val << std::endl;
+            } else {
+                sub->next = cur;
+                sub = sub->next;
+                std::cout << "sub->val = " << sub->val << std::endl;
+            }
+            cur = cur->next;
+        }
+        sub->next = nullptr;
+        if (pre && dummy) pre->next = dummy->next;
+
+        return new_head->next;
+    }
+};
